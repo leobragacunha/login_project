83a419c04602ba6c865d344f3bf1d6034cfa8620
leobragacunha <leobragacunha@hotmail.com>
2023-12-31 18:22:40 -0300

    Index page created

--- login_project_site/login_app/views.py
+++ login_project_site/login_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
 
 # Create your views here.
+def indexview(request):
+    return render(request, "index.html")
+
+
+class SiteLogin(LoginView):
+    template_name = "login_app/login.html"
+    next_page = "login_app/login_index.html"
